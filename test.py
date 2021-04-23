1bd988696ae26adf88ccb7b963b541f126137d62
abigailnm111 <60757439+abigailnm111@users.noreply.github.com>
2021-04-22 22:37:03 -0700

    working word offer letter

--- test.py
+++ test.py
@@ -16,21 +16,26 @@ new_doc=Document('Letterhead.docx')
 last_name="George"
 first_name="Regina"
 
-new_doc.save(last_name+"_"+first_name+'.docx')
-date= date.today()
+today= date.today()
+
+date= today.strftime("%B %d, %Y")
 title_code="1630"
 start= "7/1/2021"
 end= "6/30/2022"
-percentage="100%"
+percentage="100"
 annual= "60,000"
 monthly="5,000"
 courses= "3 EC 3 ksldjfskjfsldkfj"
 
-data={"<Date>":date ,"<Name>":first_name+" "+last_name, 
+data={"<Date>":str(date) ,"<Name>":first_name+" "+last_name, 
       "<Title Code>": title_code, "<start>": start, "<end>": end, 
       "<percentage>":percentage, "<annual>":annual, "<monthly>": monthly,
-      "<List of Classes& Course Titles>": courses}
+      "<List of Classes & Course Titles>": courses, "\n\n": '\n'}
 
+for key in data:
+    template=re.sub(key, data[key] , template)
 
-template=re.sub("<First Name><Last Name>", first_name+" "+last_name, template)
-print(template)
+letter=new_doc.add_paragraph(template)
+letter_format= letter.paragraph_format
+letter_format.line_spacing=1.0
+new_doc.save(last_name+"_"+first_name+'.docx')
